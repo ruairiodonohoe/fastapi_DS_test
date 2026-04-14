5603c8df927fb90560e2cd6b39fcd07fd57fa65a
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-04-14 14:14:10 +0100

    Second Commit

---
+++ project/settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    debug: bool = False
+    environment: str
+    database_url: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
